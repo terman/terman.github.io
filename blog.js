a6d00fe758fa5171a77e0c3a0e3ba500878480b9
Chris Terman <cjt@mit.edu>
2016-06-11 11:50:08 -0400

    summer 2015 revamp

---
+++ blog.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $('.entry').each(function (index,div) {
+        div = $(div);
+        var txt = div.text();
+        var title = div.attr('title');
+        var locn = div.attr('location');
+        var pix = div.attr('pix');
+        div.empty();
+
+        if (title) {
+            div.append('<p><hr></p>');
+            var t = $('<b></b>');
+            t.append(title);
+            if (locn) t.append(' &mdash; ',locn);
+            div.append($('<p></p>').append(t));
+        }
+        if (txt) {
+            div.append('<p>'+txt+'</p>');
+        }
+        $.each(pix.split(','),function (index,src) {
+            div.append('<p><img src="'+src+'"/ width="640"></p>');
+        });
+    });
+
+    $('.blurb').each(function (index,div) {
+        div = $(div);
+        var txt = div.text();
+        var title = div.attr('title');
+        var pix = div.attr('pix');
+        var url = div.attr('url');
+        div.empty();
+
+        div.append($('<img/>').attr('src',pix));
+        var link = $('<a></a>').attr('href',url).append($('<b></b>').append(title));
+        div.append(link);
+        div.append($('<p></p>').append(txt));
+    });
+
+    $('body').each(function (index,body) {
+        body = $(body);
+        var div = $('<div class="page"></div>').append(body.html());
+        body.empty().append(div);
+    });
+});
